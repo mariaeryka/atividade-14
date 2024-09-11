49ea77f176ad8e0c8011f5af8d76abe1822feef6
mariaeryka <erykamariab4@gmail.com>
2024-09-11 15:23:46 -0300

    tres notas

---
+++ att 14.py
@@ -0,0 +1,18 @@
+# Crie um programa que receba três notas de um aluno e calcule a média. Informe se o aluno foi aprovado, reprovado ou se está de recuperação. Use as seguintes regras:
+# Média ≥ 7: Aprovado
+# 5 ≤ Média < 7: Recuperação
+# Média < 5: Reprovado
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
+    
